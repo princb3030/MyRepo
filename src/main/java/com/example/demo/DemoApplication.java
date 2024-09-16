54a711cd3163f4e3ce9f12537df7209f0a4e5b16
princb3030 <princraj.30@gmail.com>
2024-09-16 16:08:54 +0530

    open/lates/pr: this is latest open pr(don't delete it)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		//this branch will be merged
+		//this is latest open pr(don't delete it)
 	}
 
 }
